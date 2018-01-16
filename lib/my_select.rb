c294182640758a7b3d580866c7615d453b6c0079
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-16 14:29:30 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,9 @@
 def my_select(collection)
   if block.given? == false 
     puts "Please enter an array"
- new_array = 0 
- i = 0 
- while i < collection.length 
- 
+  elsif 
+  new_array = 0 
+  i = 0 
+  while i < collection.length 
+  end 
 end
